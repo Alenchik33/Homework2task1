d03c30452f2a113fe4660d023628aa5efb58747a
Alenchik <govorukhina.84@mail.ru>
2023-05-04 14:58:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13_676;
+        int bonusmil = 20;
+        int bonus = price / bonusmil;
+        System.out.println("Количество начисленных миль:" + bonus);
+    }
+}
